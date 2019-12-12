a55d6000ba30b7220ad955b22db2030a1a8b5361
Francesco <franc.ns@gmail.com>
2019-12-12 21:46:29 +0100

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Charts</title>
+
+  <!-- JS: JQUERY -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- JS: MOMENT -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+  <!-- JS: CHART-JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+  <!-- CSS: MY STYLE -->
+  <link rel="stylesheet" href="style.css">
+  <!-- JS: MY SCRIPT -->
+  <script src="script.js" charset="utf-8"></script>
+
+</head>
+
+<body>
+  
+  <div class="container"></div>
+
+</body>
+
+</html>
